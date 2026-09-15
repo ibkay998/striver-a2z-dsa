d8b58d68c7c727f65bd19aa094b60d259ce02760
Evwrujae Oghenemarho <evwrujaemarho@gmail.com>
2026-09-15 23:57:15 +0300

    014-pattern-3

--- 01-learn-the-basics/03-patterns/014-pattern-3.js
+++ 01-learn-the-basics/03-patterns/014-pattern-3.js
@@ -12,8 +12,18 @@
 
 'use strict';
 
-function solve() {
-  // TODO: Implement and test the solution.
-}
+class Solution {
+    pattern3(n) {
+        let row = "";
 
-solve();
+        for (let i = 1; i <= n; i++) {
+            for (let j = 1; j <= i; j++) {
+                row += j 
+            }
+            console.log(row);
+            row = "";
+        }
+    }
+}
+let solution = new Solution();
+solution.pattern3(5);
